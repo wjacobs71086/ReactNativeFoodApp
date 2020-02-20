b5c05b7640f317421d78fcf34f1b1bbbb972f64a
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-02-20 15:26:30 -0800

    auto commit

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,38 +1,45 @@
-import React, { useState} from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import SearchBar from '../components/SearchBar';
-import Yelp from '../api/Yelp';
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
+import Yelp from "../api/Yelp";
 
 const SearchScreen = () => {
-  const [term, setTerm ] = useState('');
+  const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
   const searchApi = async () => {
-      const response = await Yelp.get('/search', {
+    setLoading(true);
+    try {
+      const response = await Yelp.get("/search", {
         // this will append ?limit=50 to the search URL
         params: {
-          limit: 50
+          limit: 50,
+          term,
+          location: "los angeles"
         }
       });
-      setResults(response.data.businesses)
-  }
-
+      setResults(response.data.businesses);
+      setLoading(false); 
+    } catch(err) {
+      console.log({error: err});
+      setLoading(false);
+      setError(`${err}`);
+    }
+  };
 
   return (
     <View>
       <Text> This is my Search Screen </Text>
-      <SearchBar 
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={searchApi}
-      />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {loading ? <Text>Loading...</Text> : null}
+      {error ? <Text>{error}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({
-  
-})
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
